dee65c77175c75696148c022786a049a022a84a1
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-09-18 20:27:06 +0530

    create client side ui using react vite

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ThirdwebProvider } from '@thirdweb-dev/react'
 import { Sepolia } from "@thirdweb-dev/chains";
-
+import './index.css';
 import App from './App'
 
 const root = ReactDom.createRoot(document.getElementById('root'))
